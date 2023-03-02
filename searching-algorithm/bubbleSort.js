f2457c5b0a97fdc5b79c9f466b46a06df317cbc6
OlojaMuyiwa <olojam4969@gmail.com>
2023-03-02 11:28:39 +0100

    bubble sorting

---
+++ searching-algorithm/bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const items =  [9, 8, 7, 6, 5, 4, 3, 2, 1];
+bubbleSort(items);
+console.log(items);
